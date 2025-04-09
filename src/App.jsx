d5a42d02d67196281a7a267eb858afa594bcb124
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-09 14:18:01 +0100

    code refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import "./App.css";
-import MovieCard from "./components/MovieCard";
 
 function App() {
-  const movieNumber = 1;
-  return (
-    <>
-      {movieNumber === 1 ? (
-        <MovieCard movie={{ title: "Tim's Film", release_date: "2024" }} />
-      ) : (
-        <MovieCard movie={{ title: "Joe's Film", release_date: "2024" }} />
-      )}
-    </>
-  );
+  return <></>;
 }
 
 export default App;
