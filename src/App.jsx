4d1bd184462685143d79bd4ccc9a887a7105db77
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-09 11:22:29 +0100

    conditionally renering the movieCard

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import "./App.css";
 import MovieCard from "./components/MovieCard";
 
 function App() {
+  const movieNumber = 1;
   return (
     <>
-      <MovieCard movie={{ title: "Tim's Film", release_date: "2024" }} />
+      {movieNumber === 1 ? (
+        <MovieCard movie={{ title: "Tim's Film", release_date: "2024" }} />
+      ) : (
+        <MovieCard movie={{ title: "Joe's Film", release_date: "2024" }} />
+      )}
     </>
   );
 }
