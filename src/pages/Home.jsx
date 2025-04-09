a0ca45486d19c3014cd4486502a9518cf0b5034a
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-09 14:14:23 +0100

    updating the Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import MovieCard from "../components/MovieCard";
+
+function Home() {
+  const movies = [
+    { id: 1, title: "John Wick", release_date: "2020" },
+    { id: 1, title: "Terminator", release_date: "1997" },
+    { id: 1, title: "The Matrix", release_date: "1998" },
+  ];
+
+  return (
+    <div className="home">
+      <div className="movie-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
