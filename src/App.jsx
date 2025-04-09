c62c63b549bd8c5ea52a7ffd55cf7305addfa38e
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-09 14:27:44 +0100

    adding a form to the page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,17 @@ import "./App.css";
 import Home from "./pages/Home";
 
 function App() {
+  const handlSearch = () => {};
+
   return (
     <>
+      <form onSubmit={handlSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="input-search"
+        />
+      </form>
       <Home />
     </>
   );
