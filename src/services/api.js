1ebffbeeeab4c18f3e2c58ca50ac5d23ff90b6cc
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-10 13:23:26 +0100

    exporting both functions

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,13 @@
 const API_KEY = "c22e9cfc323054beebbaf3b87f38e3d4";
 const BASE_URL = "https://www.themoviedb.org/";
 
-const getPopularMovies = async () => {
+export const getPopularMovies = async () => {
   const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
   const data = await response.json();
   return data.results;
 };
 
-const searchMovies = async (query) => {
+export const searchMovies = async (query) => {
   const response = await fetch(
     `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
       query
