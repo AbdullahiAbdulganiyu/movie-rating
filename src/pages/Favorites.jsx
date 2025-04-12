2da4f9cf9f29426ea3ee48229ac85b293c80aebf
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-12 15:03:11 +0100

    returning a movie if the movie exist in the favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,9 +1,21 @@
+import MovieCard from "../components/MovieCard";
 import { useMovieContext } from "../contexts/MovieContext";
 import "../css/Favorites.css";
 
 function Favorites() {
   const { favorites } = useMovieContext();
 
+  if (favorites)
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="movie-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    );
   return (
     <div className="favorites-empty">
       <h2>No Favorite Movies yet</h2>
